90dedff86a3f695d027e6d5f774826aecf1295f7
Sotatsu57 <tassei.moriyama@gmail.com>
2022-05-18 16:26:25 +0900

    commit---

--- works/counter.js
+++ works/counter.js
@@ -5,3 +5,8 @@ const countUp = () => {
   const countElement = document.querySelector('#count')
   countElement.innerText = '回数: ' + count
 }
+const countReset = () => {
+    count = 0
+    const countElement = document.querySelector('#count')
+    countElement.innerText = '回数: ' + count
+  }
